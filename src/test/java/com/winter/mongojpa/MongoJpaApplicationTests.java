1198e7cf50f214ba4af7f1b1ae13340b74f75bab
Jack <jackwinter.10@gmail.com>
2019-07-12 18:55:25 +0100

    Added docker image for mongodb

--- src/test/java/com/winter/mongojpa/MongoJpaApplicationTests.java
+++ src/test/java/com/winter/mongojpa/MongoJpaApplicationTests.java
@@ -1,18 +1,13 @@
 package com.winter.mongojpa;
 
-import com.winter.mongojpa.repository.PostRepository;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class MongoJpaApplicationTests {
-
-    @Autowired
-    private PostRepository postRepository;
 
     @Test
     public void contextLoads() {
