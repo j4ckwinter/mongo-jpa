5858bbdb0b073e0de47694158870b5cd5b4282e7
winter <jackwinter.10@gmail.com>
2019-07-12 16:27:49 +0100

    Began post service test for updating a post

--- src/test/java/com/winter/mongojpa/service/PostServiceTest.java
+++ src/test/java/com/winter/mongojpa/service/PostServiceTest.java
@@ -2,6 +2,7 @@ package com.winter.mongojpa.service;
 
 import com.winter.mongojpa.model.Post;
 import com.winter.mongojpa.repository.PostRepository;
+import org.bson.types.ObjectId;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
@@ -35,6 +36,29 @@ public class PostServiceTest {
 
         //then
         assertThat(createdPost.getTitle()).isSameAs(dummyPost.getTitle());
+    }
+
+    @Test
+    public void whenEditingAPostIStayEdited() {
+        //given
+        Post dummyPost = new Post();
+        ObjectId objectId = new ObjectId();
+        dummyPost.set_id(objectId);
+        dummyPost.setTitle("I'm a postman");
+        dummyPost.setBody("Like a postman I always deliver");
+        dummyPost.setUserId(999L);
+        dummyPost.setPostId(989L);
+
+        //when
+        when(mockPostRepository.save(any(Post.class))).thenReturn(dummyPost);
+        Post createdPost = postService.createPost(dummyPost);
+        createdPost.setBody("Bloody hell I forgot to deliver");
+        when(mockPostRepository.save(any(Post.class))).thenReturn(createdPost);
+        postService.updatePost(objectId, createdPost);
+
+        //then
+
+
     }
 
     @Test(expected = NullPointerException.class)
