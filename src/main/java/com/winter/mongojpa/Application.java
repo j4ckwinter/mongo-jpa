0f5bfb957ab00c4801b010f0e1ffbd1441eb568d
Jack <jackwinter.10@gmail.com>
2019-07-13 17:22:45 +0100

    Added spring actuator with security

--- src/main/java/com/winter/mongojpa/Application.java
+++ src/main/java/com/winter/mongojpa/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MongoJpaApplication {
+public class Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(MongoJpaApplication.class, args);
+        SpringApplication.run(Application.class, args);
     }
 
 }
